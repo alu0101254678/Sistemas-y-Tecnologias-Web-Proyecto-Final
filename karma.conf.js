1ace11126f34c60fdf7b76841248a16f5dfd97fd
Yago Pérez Molanes <alu0101254678@ull.edu.es>
2021-12-24 10:19:01 +0000

    travis

--- karma.conf.js
+++ karma.conf.js
@@ -40,11 +40,5 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true,
-    customLaunchers: {
-      ChromeHeadlessPuppeteer: {
-        base: 'ChromeHeadless',
-        flags: [ '--no-sandbox', '--disable-gpu' ]
-      }
-    }
   });
 };
